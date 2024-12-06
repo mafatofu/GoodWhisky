96df3871a18208bcb22795fe02256027a623c3cc
mafatofu <mafatofu@github.com>
2024-12-06 16:10:47 +0900

    build

---
+++ src/main/java/org/example/goodwhisky/GoodWhiskyApplication.java
@@ -0,0 +1,13 @@
+package org.example.goodwhisky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodWhiskyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodWhiskyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/goodwhisky/GoodWhiskyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.goodwhisky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodWhiskyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
